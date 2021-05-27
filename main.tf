36da345d2a52d8418c5a05ba04f7bd4ae44139c9
Romulo Miranda <romulobmiranda@gmail.com>
2021-05-26 22:17:05 -0400

    Úlltima versão

--- main.tf
+++ main.tf
@@ -76,6 +76,12 @@ resource "tls_private_key" "example" {
   rsa_bits  = 4096
 }
 
+resource "local_file" "private_key" {
+  content = tls_private_key.example.private_key_pem
+  filename = "private_key.pem"
+  file_permission = "0600"
+}
+
 resource "aws_key_pair" "generated_key" {
   key_name   = var.key_name
   public_key = tls_private_key.example.public_key_openssh
