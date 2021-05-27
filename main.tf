7f6a82da29fba2f0df955aaf72b0e599ba2889b1
Romulo Miranda <romulobmiranda@gmail.com>
2021-05-27 12:51:28 -0400

    Update code

--- main.tf
+++ main.tf
@@ -25,7 +25,6 @@ resource "aws_vpc" "vpc-bastion" {
 
 #CRIAR PEERING ENTRE VPCS
 resource "aws_vpc_peering_connection" "conexao-vpcs" {
-  peer_owner_id = var.peer_owner_id
   peer_vpc_id = aws_vpc.vpc-bootcamp.id
   vpc_id = aws_vpc.vpc-bastion.id
   auto_accept = true
